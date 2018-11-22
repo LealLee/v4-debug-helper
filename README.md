72b172f9360d0a20f7f59cd9733c858cb45e8b02
LealLee <lil@tradeshift.com>
2018-11-22 18:32:43 +0800

    support redirect multi local v4 apps

--- README.md
+++ README.md
@@ -16,28 +16,43 @@ npm install
 ```
 ## 启动
 
-1. 首先修改`config.json`文件里面本地的V4路径，APPS项目用：
-    ```
-    {
-        "redirectV4AppIndexUrl": "http://localhost:8321"
-    }
-    ```
-    cn-pay-apps项目用：
-    ```
-    {
-        "redirectV4AppIndexUrl": "http://localhost:8844"
-    }
-    ```
+1. 首先修改`config.json`文件里面`redirectV4AppIndexUrl`配置,
+
+```
+"redirectV4AppIndexUrl": {
+    "http://localhost:8321": [
+        "DEFAULT"
+    ],
+    "http://localhost:8844": [
+        "FapiaoManager",
+        "CNPayTenantAdmin",
+        ......
+    ],
+    "http://localhost:8102": [
+        "BrandhouseLogisticsManager", 
+        "PriceComparison", 
+        ...
+    ]
+}
+```
+- `redirectV4AppIndexUrl`里面的key是本地V4项目的启动端口
+- key对应的数组里面值是你想要debug的app的id.
+- 如果在所有的数组中都没找到当前的appid, 那么默认用`DEFULAT`对应的转发地址`http://localhost:8321` 
+
+
 2. 运行`npm start`，将会启动两个端口
 * 8001: http(s)代理服务器端口
 * 8002: 代理服务器web配置页面端口
 
 ## 使用
-1. 下载安装证书
+1. 安装证书
 
-    1.1 [下载](http://localhost:8002/fetchCrtFile)
-        
-    1.2 双击安装, [不会点我](http://anyproxy.io/cn/#osx%E7%B3%BB%E7%BB%9F%E4%BF%A1%E4%BB%BBca%E8%AF%81%E4%B9%A6)
+    1.1 生成证书
+        运行`npx anyproxy-ca`, 然后自动打开证书目录。
+
+    1.2 信任证书
+        参考 [OSX系统信任CA证书](http://anyproxy.io/cn/#osx%E7%B3%BB%E7%BB%9F%E4%BF%A1%E4%BB%BBca%E8%AF%81%E4%B9%A6)
+    
 
 2. 为浏览器设置代理
 
--- ruleToReplaceAppIndex.js
+++ ruleToReplaceAppIndex.js
@@ -1,22 +1,38 @@
 const config = require('./config.json');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const redirectV4Config = {
-    index: config.redirectV4AppIndexUrl
-};
 
 const hackUrlReg = /^https:\/\/.+\.bwtsi\.cn\/v4\/apps\/\w+\.\w+$/;
 
 const getConfigScript = html => html.match(/<script type="text\/javascript">\s+var __config = \{.+\};\s+<\/script>/)[0];
 
-const insertConfig = (html, configScript) => {
+const getAppNameFromUrl = requestUrl => requestUrl.match(/Tradeshift\..+/)[0].replace('Tradeshift.', '');
+
+// 重新组织config.json里面的config.redirectV4AppIndexUrl数据
+// 返回值类型是{'DEFAULT': 'redirect_url', appName: 'redirect_url', ...}
+const prepareRedirectData = () => {
+    const appUrlMap = {};
+    Object.keys(config.redirectV4AppIndexUrl).forEach(url => {
+        config.redirectV4AppIndexUrl[url].forEach(appName => appUrlMap[appName] = url);
+    });
+    return appUrlMap;
+};
+
+const appRedirectMapping = prepareRedirectData();
+
+const getRedirectUrl = sourceUrl => {
+    const appName = getAppNameFromUrl(sourceUrl);
+    return appRedirectMapping[appName] || appRedirectMapping['DEFAULT'];
+}
+
+const insertConfig = (html, configScript, redirectIndex) => {
     const $ = cheerio.load(html, {xmlMode: true});
     $('body').prepend(configScript);
     $('*').each((index, element) => {
         if (element.name === 'script') {
             const attrSrc = $(element).attr('src');
             if (attrSrc && (attrSrc.indexOf('/webpack') === 0 || attrSrc.indexOf('/locale.js') === 0 || attrSrc.indexOf('/bundles') === 0)) {
-                $(element).attr('src', redirectV4Config.index + attrSrc);
+                $(element).attr('src', redirectIndex + attrSrc);
             }
         }
         if (!$(element).html()) {
@@ -32,10 +48,11 @@ module.exports = {
     *beforeSendResponse(requestDetail, responseDetail) {
 
         if(hackUrlReg.test(requestDetail.url)) {
+            const redirectIndex = getRedirectUrl(requestDetail.url)
             const newResponse = responseDetail.response;
             const body = newResponse.body.toString();
             const originalConfigScript = getConfigScript(body);
-            const redirectAppUrl = `${redirectV4Config.index}${requestDetail.requestOptions.path.replace('/v4','')}`;
+            const redirectAppUrl = redirectIndex + requestDetail.requestOptions.path.replace('/v4','');
             const logStr = `redirect ${requestDetail.url} to ${redirectAppUrl}`;
 
             console.time(logStr);
@@ -43,7 +60,7 @@ module.exports = {
 
             return rp(redirectAppUrl).then(html => {
                 let newRedirectHtml = html.replace('__config', '__configOriginal');
-                newResponse.body = insertConfig(newRedirectHtml, originalConfigScript);
+                newResponse.body = insertConfig(newRedirectHtml, originalConfigScript, redirectIndex);
                 console.timeEnd(logStr);
                 return {response: newResponse};
             }).catch(err => {
